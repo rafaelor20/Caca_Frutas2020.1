e6b2e98e0146f13262e6fe8515c7c8228bded2f3
rafael <rafael@rafael-AMD-Desktop>
2020-11-25 12:16:06 -0300

    commit teste

--- src/cata_frutas/Fruta.java
+++ src/cata_frutas/Fruta.java
@@ -20,52 +20,52 @@ public class Fruta {
         this.tipo = tipo;
         switch (this.tipo){
             case "maracuja":
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 1;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 1;
+                this.antidoto = false;
            
             case "coco":
-            this.força = 1;
-            this.agilidade = 2;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 2;
+                this.pontos = 0;
+                this.antidoto = false;
             
             case "abacate":
-            this.força = 2;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 2;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = false;
             
             case "laranja":
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = true;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = true;
             
             case "morango":
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = false;
             
             case "amora":
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = false;
             
             case "uva":
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = false;
             
             case default:
-            this.força = 1;
-            this.agilidade = 1;
-            this.pontos = 0;
-            this.antidoto = false;
+                this.força = 1;
+                this.agilidade = 1;
+                this.pontos = 0;
+                this.antidoto = false;
         }
     }
 }
